1abb1b6850074526054f89f5a6f65fc90e00cd9f
talterici <talterici@gmail.com>
2019-09-27 14:18:15 -0400

    Initial commit

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-cloak
+      id="app">
+     <app-banner/>
+     <app-navbar />
+     <router-view />
+     <app-footer />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>

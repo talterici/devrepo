9a02bdb659b7ac6b04d3d222976f19342e32c8e2
talterici <talterici@gmail.com>
2019-09-27 14:31:07 -0400

    Add files via upload

---
+++ src/AppBanner.vue
@@ -0,0 +1,16 @@
+<template functional>
+    <div>
+            <h6>Banner</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppBanner'.
+
+    data() {
+        return {};
+    };
+
+}
+</script>
